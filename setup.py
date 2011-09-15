f1fcea83782edc30747c7d112fda6952b655df95
Mark Shroyer <code@markshroyer.com>
2011-09-15 19:29:11 -0400

    Switch to setuptools
    Change the setup.py script from distutils to setuptools, and move the
    module .py file into the distribution root directory.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pointfree',
       version='0.1',
       description='Functional programming toolkit for Python',
+      long_description=read('README.md'),
+      license="BSD",
       author='Mark Shroyer',
       author_email='code@markshroyer.com',
       url='https://github.com/markshroyer/python-pointfree',
       py_modules=['pointfree'],
-      package_dir = {'': 'src'}
+      test_suite='test'
       )
