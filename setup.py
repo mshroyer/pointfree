fd76534b4c0ab5b003088d064633c578b21b1d08
Mark Shroyer <code@markshroyer.com>
2011-09-14 23:23:51 -0400

    Add distutils setup.py
    Added setup.py and moved the fpkit module into the src subdirectory.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fpkit',
+      version='0.1',
+      description='Functional programming toolkit for Python',
+      author='Mark Shroyer',
+      author_email='code@markshroyer.com',
+      url='https://github.com/markshroyer/fpkit',
+      py_modules=['fpkit'],
+      package_dir = {'': 'src'}
+      )
