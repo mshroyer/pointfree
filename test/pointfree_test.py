59a0255babaef772ad72e36c688e72b3385b0db4
Mark Shroyer <code@markshroyer.com>
2011-09-15 19:30:11 -0400

    Add simple unit test module

---
+++ test/pointfree_test.py
@@ -0,0 +1,21 @@
+import os, sys, unittest
+from pointfree import *
+
+class CurryingTest(unittest.TestCase):
+    def testCurrying1(self):
+        @curryable
+        def add(a, b):
+            return a + b
+
+        @curryable
+        def mult(a, b):
+            return a * b
+
+        add1  = add(1)
+        mult2 = mult(2)
+
+        self.assertEqual(add1(3), 4)
+        self.assertEqual(mult2(4), 8)
+
+if __name__ == '__main__': 
+   unittest.main()
