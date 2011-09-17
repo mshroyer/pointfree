a0b56ee21602786890b3217d843e6884432d2367
Mark Shroyer <code@markshroyer.com>
2011-09-17 16:27:48 -0400

    Flesh out currying unit tests
    Check for error conditions and edge cases in the function currying unit
    test suite.

--- test/pointfree_test.py
+++ test/pointfree_test.py
@@ -1,18 +1,60 @@
-import os, sys, unittest
+import os, sys, unittest, types
 from pointfree import *
 
-class CurryingTest(unittest.TestCase):
-    def testCurrying1(self):
-        @curryable
-        def add(a, b):
-            return a + b
+@curryable
+def add(a, b):
+    return a + b
 
-        @curryable
-        def mult(a, b):
-            return a * b
+@curryable
+def mul(a, b):
+    return a * b
+
+class CurryingTest(unittest.TestCase):
+    """Function currying tests"""
+
+    def testCurryingSimple(self):
+        """Check that basic partial application works"""
 
         add1  = add(1)
-        mult2 = mult(2)
-
         self.assertEqual(add1(3), 4)
-        self.assertEqual(mult2(4), 8)
+
+        mul2 = mul(2)
+        self.assertEqual(mul2(4), 8)
+
+    def testCurryingSingleApplication(self):
+        """Check single application of a curried function
+
+        We must still be able to apply a curried function to all its
+        arguments simultaneously.
+
+        """
+
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(mul(2, 3), 6)
+
+    def testCurryingTooManyArgs(self):
+        """Error handling: too many arguments
+
+        An exception should be thrown if the curried function is applied to
+        an argument list longer than it can accept.
+
+        """
+
+        with self.assertRaises(TypeError):
+            add(1, 2, 3)
+
+    def testCurryingNoArgs(self):
+        """Edge case: zero arguments applied
+
+        The curryable function should return a functionally (though not
+        necessarily referentially identical) function if it is applied to
+        an empty argument list one or more times.
+
+        """
+
+        f = add()
+        self.assertEqual(f(1, 2), 3)
+
+        g = mul()()()
+        self.assertEqual(g(3, 4), 12)
+        
