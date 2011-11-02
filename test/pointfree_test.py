bd55f03b2e20469ce8c32a2a2496e074b530a293
Mark Shroyer <code@markshroyer.com>
2011-11-02 01:52:26 -0400

    New @partial tests

--- test/pointfree_test.py
+++ test/pointfree_test.py
@@ -3,86 +3,54 @@
 import os, sys, unittest, types
 from pointfree import partial, pointfree
 
-### CURRYING TESTING ######################################################
+### PARTIAL APPLICATION TESTS #############################################
 
-@pointfree
-def currying_add(a, b, c):
-    return a + b + c
+@partial
+def partial_adder(a, b, c):
+    return a + 2*b + 3*c
 
-@pointfree
-def currying_mul(a, b, c):
-    return a * b * c
+@partial
+def partial_adder_defaults(a, b, c=3):
+    return a + 2*b + 3*c
 
-class CurryingThing(object):
-    """Class test fixture for @pointfree decorator"""
-
-    m = 3
+class PartialThing(object):
+    m = 1
 
     def __init__(self, n):
         self.n = n
 
-    @pointfree
-    def instance_add(self, a, b, c):
-        return self.n + a + b + c
+    @partial
+    def instance_adder(self, a, b, c):
+        return self.n + a + 2*b + 3*c
 
-    @pointfree
-    def instance_mul(self, a, b, c):
-        return self.n * a * b * c
-
-    @pointfree
+    @partial
     @classmethod
-    def class_add(klass, a, b, c):
-        return klass.m + a + b + c
+    def class_adder(klass, a, b, c):
+        return klass.m + a + 2*b + 3*c
 
-    @pointfree
-    @classmethod
-    def class_mul(klass, a, b, c):
-        return klass.m * a * b * c
-
-    @pointfree
+    @partial
     @staticmethod
-    def static_add(a, b, c):
-        return a + b + c
+    def static_adder(a, b, c):
+        return a + 2*b + 3*c
 
-    @pointfree
-    @staticmethod
-    def static_mul(a, b, c):
-        return a * b * c
-        
-class SimpleCurryingFunctionCase(unittest.TestCase):
-    def testFunctions(self):
-        f = currying_add(1, 2)
-        self.assertEqual(f(3), 6)
+class PartialFunctionCase(unittest.TestCase):
+    def testNormalApplication(self):
+        self.assertEqual(partial_adder(1,2,3), 14)
+        self.assertEqual(partial_adder(a=1,b=2,c=3), 14)
 
-class CurryingFunctionCase(unittest.TestCase):
-    def testFunctions(self):
-        f = currying_add(1, 2) * currying_mul(3, 4)
-        self.assertEqual(f(2), 27)
+    def testPartialApplication(self):
+        self.assertEqual(partial_adder(1)(2)(3), 14)
+        self.assertEqual(partial_adder(1,2)(3), 14)
+        self.assertEqual(partial_adder(1)(2,3), 14)
+        self.assertEqual(partial_adder()(1)()(2)()(3), 14)
 
-class CurryingInstanceCase(unittest.TestCase):
-    def setUp(self):
-        self.t = CurryingThing(2)
+    def testPartialKeywords(self):
+        self.assertEqual(partial_adder(c=3)(1,2), 14)
 
-    def testInstanceMethods(self):
-        f = self.t.instance_add(1, 2) * self.t.instance_mul(3, 4)
-        self.assertEqual(f(2), 53)
-
-    def testClassMethods(self):
-        f = self.t.class_add(1, 2) * self.t.class_mul(3, 4)
-        self.assertEqual(f(2), 78)
-
-    def testStaticMethods(self):
-        f = self.t.static_add(1, 2) * self.t.static_mul(3, 4)
-        self.assertEqual(f(2), 27)
-
-class CurryingClassCase(unittest.TestCase):
-    def testClassMethods(self):
-        f = CurryingThing.class_add(1, 2) * CurryingThing.class_mul(3, 4)
-        self.assertEqual(f(2), 78)
-
-    def testStaticMethods(self):
-        f = CurryingThing.static_add(1, 2) * CurryingThing.static_mul(3, 4)
-        self.assertEqual(f(2), 27)
+class PartialFunctionDefaultsCase(unittest.TestCase):
+    def testNormalApplication(self):
+        self.assertEqual(partial_adder_defaults(1,2), 14)
+        self.assertEqual(partial_adder_defaults(1,2,4), 17)
 
 if __name__ == '__main__':
     unittest.main()
