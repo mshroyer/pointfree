38abea8c0bfe362b2bd16fdca3402bc92b3da3f4
Mark Shroyer <code@markshroyer.com>
2011-11-03 00:26:01 -0400

    Unit tests for variable arguments
    Add unit tests for variable argument lists for partially-applied functions,
    and break out some of the other test functions into separate, smaller tests
    as appropriate.

--- test/pointfree_test.py
+++ test/pointfree_test.py
@@ -6,12 +6,16 @@ from pointfree import partial, pointfree
 ### PARTIAL APPLICATION TESTS #############################################
 
 @partial
-def partial_adder(a, b, c):
+def padd(a, b, c):
     return a + 2*b + 3*c
 
 @partial
-def partial_adder_defaults(a, b, c=3):
+def padd_defaults(a, b, c=3):
     return a + 2*b + 3*c
+
+@partial
+def padd_var_args(a, b, *args):
+    return a + 2*b + 3*sum(args)
 
 class PartialThing(object):
     m = 1
@@ -33,24 +37,42 @@ class PartialThing(object):
     def static_adder(a, b, c):
         return a + 2*b + 3*c
 
-class PartialFunctionCase(unittest.TestCase):
+class PartialFuncCase(unittest.TestCase):
     def testNormalApplication(self):
-        self.assertEqual(partial_adder(1,2,3), 14)
-        self.assertEqual(partial_adder(a=1,b=2,c=3), 14)
+        self.assertEqual(padd(1,2,3), 14)
+
+    def testNormalKeywordApplication(self):
+        self.assertEqual(padd(a=1,b=2,c=3), 14)
 
     def testPartialApplication(self):
-        self.assertEqual(partial_adder(1)(2)(3), 14)
-        self.assertEqual(partial_adder(1,2)(3), 14)
-        self.assertEqual(partial_adder(1)(2,3), 14)
-        self.assertEqual(partial_adder()(1)()(2)()(3), 14)
+        self.assertEqual(padd(1)(2)(3), 14)
+
+    def testClusteredPartialApplication(self):
+        self.assertEqual(padd(1,2)(3), 14)
+        self.assertEqual(padd(1)(2,3), 14)
+
+    def testInterspersedPartialApplication(self):
+        self.assertEqual(padd()(1)()(2)()(3), 14)
 
     def testPartialKeywords(self):
-        self.assertEqual(partial_adder(c=3)(1,2), 14)
+        self.assertEqual(padd(c=3)(1,2), 14)
 
-class PartialFunctionDefaultsCase(unittest.TestCase):
+class PartialFuncDefaultsCase(unittest.TestCase):
     def testNormalApplication(self):
-        self.assertEqual(partial_adder_defaults(1,2), 14)
-        self.assertEqual(partial_adder_defaults(1,2,4), 17)
+        self.assertEqual(padd_defaults(1,2), 14)
+
+    def testPositionalOverride(self):
+        self.assertEqual(padd_defaults(1,2,4), 17)
+
+    def testKeywordOverride(self):
+        self.assertEqual(padd_defaults(c=4)(1,2), 17)
+
+class PartialFuncVarArgsCase(unittest.TestCase):
+    def testNormalApplication(self):
+        self.assertEqual(padd_var_args(1,2), 5)
+
+    def testVarArgsApplication(self):
+        self.assertEqual(padd_var_args(1,2,3,4), 26)
 
 if __name__ == '__main__':
     unittest.main()
