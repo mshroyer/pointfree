01159652e1f6199bc1c8f7f26cea049ae6c41c71
Mark Shroyer <code@markshroyer.com>
2011-11-05 01:31:14 -0400

    Revert back to distutils
    Switch back from setuptools to distutils in order to support a wider
    audience.  Simply running
    
        python -m test.pointfree_test
    
    will suffice as a testing solution.

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup
+from distutils.core import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
 
-setup(name='pointfree',
-      version='0.1',
-      description='Functional programming toolkit for Python',
-      long_description=read('README.md'),
-      license="BSD",
-      author='Mark Shroyer',
-      author_email='code@markshroyer.com',
-      url='https://github.com/markshroyer/python-pointfree',
-      py_modules=['pointfree'],
-      test_suite='test'
-      )
+setup(
+    name='pointfree',
+    version='0.1',
+    description='Pointfree style toolkit for Python',
+    author='Mark Shroyer',
+    author_email='code@markshroyer.com',
+    url='https://github.com/markshroyer/python-pointfree',
+    py_modules=['pointfree']
+    )
