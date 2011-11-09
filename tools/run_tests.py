b77879b13972f740cc79432a546331493214a6e7
Mark Shroyer <code@markshroyer.com>
2011-11-08 23:31:12 -0500

    Move the run_tests.py script under tools

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -2,15 +2,24 @@
 
 from __future__ import print_function
 
-import unittest, doctest
-import pointfree, test.pointfree_test
+import sys, unittest, doctest
+from os.path import realpath, join, dirname
 
 if __name__ == '__main__':
+    project_path = realpath(join(dirname(__file__), '..'))
+
+    sys.path = [project_path] + sys.path
+    import pointfree, test.pointfree_test
+
     unittest_suite = unittest.TestSuite()
     unittest_suite.addTest(unittest.TestLoader().loadTestsFromModule(test.pointfree_test))
 
+    def make_docfilesuite(filename):
+        return doctest.DocFileSuite(join(project_path, "doc", filename), module_relative=False)
+
     doctest_suite = unittest.TestSuite()
-    doctest_suite.addTest(doctest.DocFileSuite("README.rst"))
+    doctest_suite.addTest(make_docfilesuite("overview.rst"))
+    doctest_suite.addTest(make_docfilesuite("faq.rst"))
     doctest_suite.addTest(doctest.DocTestSuite(pointfree))
 
     test_runner = unittest.TextTestRunner(verbosity=1)
