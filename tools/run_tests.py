f2daad0bd766df77019fd5b5bdeaf9879b50e1d2
Mark Shroyer <code@markshroyer.com>
2011-11-09 23:46:17 -0500

    Make doctest work for pointfree/partial objects
    Modify a doctest class so that documentation tests are found for pointfree
    and partial object instances, just as for functions.  This doctest
    implementation has been found to work on the following platforms:
    
      * CPython 2.6, 2.7, 3.1, and 3.2
      * PyPy 1.6.0
    
    It does *not* appear to work with CPython 3.0.

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -2,15 +2,88 @@
 
 from __future__ import print_function
 
-import sys, unittest, doctest
+import sys, unittest, doctest, inspect
 from os.path import realpath, join, dirname
 
+project_path = realpath(join(dirname(__file__), '..'))
+
+sys.path = [project_path] + sys.path
+import pointfree, test.pointfree_test
+
+class PFDocTestFinder(doctest.DocTestFinder):
+    """A modified doctest.DocTestFinder which treats partial and
+    pointfree instances as functions for the purpose of test
+    gathering.
+
+    Based on code from Python 3.2.2's doctest.py.
+    
+    """
+
+    def _find(self, tests, obj, name, module, source_lines, globs, seen):
+        """
+        Find tests for the given object and any contained objects, and
+        add them to `tests`.
+        """
+        if self._verbose:
+            print('Finding tests in %s' % name)
+
+        # If we've already processed this object, then ignore it.
+        if id(obj) in seen:
+            return
+        seen[id(obj)] = 1
+
+        # Find a test for this object, and add it to the list of tests.
+        test = self._get_test(obj, name, module, globs, source_lines)
+        if test is not None:
+            tests.append(test)
+
+        # Look for tests in a module's contained objects.
+        if inspect.ismodule(obj) and self._recurse:
+            for valname, val in obj.__dict__.items():
+                valname = '%s.%s' % (name, valname)
+                # Recurse to functions & classes.
+                if ((inspect.isfunction(val) or inspect.isclass(val) or isinstance(val, pointfree.partial)) and
+                    self._from_module(module, val)):
+                    self._find(tests, val, valname, module, source_lines,
+                               globs, seen)
+
+        # Look for tests in a module's __test__ dictionary.
+        if inspect.ismodule(obj) and self._recurse:
+            for valname, val in getattr(obj, '__test__', {}).items():
+                if not isinstance(valname, str):
+                    raise ValueError("DocTestFinder.find: __test__ keys "
+                                     "must be strings: %r" %
+                                     (type(valname),))
+                if not (inspect.isfunction(val) or inspect.isclass(val) or
+                        inspect.ismethod(val) or inspect.ismodule(val) or
+                        isinstance(val, str)):
+                    raise ValueError("DocTestFinder.find: __test__ values "
+                                     "must be strings, functions, methods, "
+                                     "classes, or modules: %r" %
+                                     (type(val),))
+                valname = '%s.__test__.%s' % (name, valname)
+                self._find(tests, val, valname, module, source_lines,
+                           globs, seen)
+
+        # Look for tests in a class's contained objects.
+        if inspect.isclass(obj) and self._recurse:
+            for valname, val in obj.__dict__.items():
+                # Special handling for staticmethod/classmethod.
+                if isinstance(val, staticmethod):
+                    val = getattr(obj, valname)
+                if isinstance(val, classmethod):
+                    val = getattr(obj, valname).__func__
+
+                # Recurse to methods, properties, and nested classes.
+                if ((inspect.isfunction(val) or inspect.isclass(val) or
+                      isinstance(val, property)) and
+                      self._from_module(module, val)):
+                    valname = '%s.%s' % (name, valname)
+                    self._find(tests, val, valname, module, source_lines,
+                               globs, seen)
+
+
 if __name__ == '__main__':
-    project_path = realpath(join(dirname(__file__), '..'))
-
-    sys.path = [project_path] + sys.path
-    import pointfree, test.pointfree_test
-
     unittest_suite = unittest.TestSuite()
     unittest_suite.addTest(unittest.TestLoader().loadTestsFromModule(test.pointfree_test))
 
@@ -19,8 +92,9 @@ if __name__ == '__main__':
 
     doctest_suite = unittest.TestSuite()
     doctest_suite.addTest(make_docfilesuite("overview.rst"))
+    doctest_suite.addTest(make_docfilesuite("api.rst"))
     doctest_suite.addTest(make_docfilesuite("faq.rst"))
-    doctest_suite.addTest(doctest.DocTestSuite(pointfree))
+    doctest_suite.addTest(doctest.DocTestSuite(pointfree, test_finder=PFDocTestFinder()))
 
     test_runner = unittest.TextTestRunner(verbosity=1)
 
