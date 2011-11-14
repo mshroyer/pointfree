8e7bff654a8a08ac8d06ed5701495697f2f550b9
Mark Shroyer <code@markshroyer.com>
2011-11-14 02:07:56 -0500

    Added long_description for PyPI

--- setup.py
+++ setup.py
@@ -6,6 +6,30 @@ setup(
     name='pointfree',
     version='1.0.0',
     description='Pointfree style toolkit for Python',
+    long_description="""
+pointfree is a small module that makes certain functional programming
+constructs more convenient to use in Python.
+
+Specifically, it provides:
+
+* A decorator to enable *automatic* partial application of functions and
+  methods.
+* Notations for function composition through operator overloading.
+* Helper functions to make composing generators more elegant.
+
+The objective is to support the `pointfree programming style`_ in a
+lightweight and easy to use manner -- and in particular, to serve as a nice
+syntax for the kind of generator pipelines described in David Beazley's
+PyCon 2008 presentation, `"Generator Tricks for Systems Programmers"`_.
+
+.. _`pointfree programming style`: http://www.haskell.org/haskellwiki/Pointfree
+
+.. _`"Generator Tricks for Systems Programmers"`: http://www.dabeaz.com/generators/Generators.pdf
+
+Full documentation is available on the web at:
+
+http://markshroyer.com/docs/pointfree/latest/
+""",
     author='Mark Shroyer',
     author_email='code@markshroyer.com',
     url='https://github.com/markshroyer/pointfree',
